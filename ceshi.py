080ff51b68cd8059265b57042265210a0595d517
QianchuanJiang <47807399+QianchuanJiang@users.noreply.github.com>
2019-08-25 21:04:22 +0800

    Update ceshi.py

--- ceshi.py
+++ ceshi.py
@@ -4,6 +4,7 @@ from Network_main import neuralNetwork
 i = 3
 h = 3
 o = 3
+learning_rate = 0.1
 
 n = neuralNetwork(inputnodes=i, hiddennodes=h, outputnodes=o, learningrate=0.3)
 print(n.query([1.0, 0.5, -1.5]))
