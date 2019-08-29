0579d0576d1e4d761b5e4ac520f4ff672bf4d73a
QianChuan J <445219842@qq.com>
2019-08-29 13:21:34 +0800

    201908261648

---
+++ Read_hdf_func.py
@@ -0,0 +1,20 @@
+from __future__ import division
+from pyhdf.SD import SD, SDC
+import numpy as np
+import pprint
+
+if __name__ == '__main__':
+    hdf_name = 'datas/MCD43A3C.A2019227.h27v05.006.2019236003508.hdf'
+    hdf_obj = SD(hdf_name)
+    print(hdf_obj.info())
+    data_dic = hdf_obj.datasets()
+    for idx,sds in enumerate(data_dic.keys()):
+        print(idx,sds)
+    bsa = hdf_obj.select('Albedo_BSA_vis')
+    data = bsa.get()
+    data = np.transpose(data)
+    print(data)
+    print(data.shape)
+
+
+
--- Train_MNIST.py
+++ Train_MNIST.py
@@ -12,7 +12,7 @@ def read_datas(data_path):
 
 if __name__ == '__main__':
     # 读取到训练集的每一行数据；
-    data_list = read_datas('mnist_train.csv')
+    data_list = read_datas('datas/mnist_train.csv')
     # # 获取第一张训练图片的全部数据；
     # all_values = data_list[100].split(',')
     # # 截取数据第一位以后的所有数据，并把他们转换成一个28*28的二维数组；
