b24ab49f8ae2dfa5c4d77c257c2804d3144543ef
QianChuan J <445219842@qq.com>
2019-08-30 16:44:06 +0800

    20190829

--- Read_hdf_func.py
+++ Read_hdf_func.py
@@ -1,6 +1,7 @@
 from __future__ import division
 from pyhdf.SD import SD, SDC
 import numpy as np
+import matplotlib.pyplot as plt
 import pprint
 
 if __name__ == '__main__':
@@ -10,12 +11,22 @@ if __name__ == '__main__':
     data_dic = hdf_obj.datasets()
     for idx,sds in enumerate(data_dic.keys()):
         print(idx,sds)
-    bsa = hdf_obj.select('Albedo_BSA_vis')
+    bsa = hdf_obj.select('Albedo_WSA_Band1')
     data = bsa.get()
     data = np.transpose(data)
-    print(data)
-    print(data.shape)
-    # ds
+    uee = [
+        [82, 18, 25, 32, 25, 14, 25],
+        [85, 18, 25, 32, 25, 14, 25],
+        [90, 18, 25, 32, 25, 14, 25],
+        [100, 18, 25, 32, 25, 14, 25],
+        [125, 18, 25, 32, 0, 14, 25],
+        [150, 18, 25, 32, 25, 14, 25],
+        [255, 18, 25, 32, 25, 14, 25]
+    ]
+    f_data = np.asfarray(data*255/32767)
+    print(f_data)
+    plt.imshow(f_data, cmap='Blues')
+    plt.show()
 
 
 
